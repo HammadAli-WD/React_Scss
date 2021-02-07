405376efad452481d1ba136b50f2d35b4642c87f
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2021-02-07 20:16:57 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import "./sass/main.scss"
-import Header from "./components/Header"
-
+import "./sass/main.scss";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Showcase from "./components/Showcase";
+import Blank from "./components/Blank";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Header />
-      
+      <About />
+      <Showcase />
+      <Blank />
+      <Portfolio />
     </div>
   );
 }
